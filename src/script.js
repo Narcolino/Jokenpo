06158dff4bd37904084bd56709a5aec5a4b41d4b
Marco Santos <marcosanto5550123@outlook.com>
2023-01-08 00:09:39 -0400

    comentado o arquivo js

--- src/script.js
+++ src/script.js
@@ -1,19 +1,23 @@
+/*declaração de variaveis */ 
 var playerpoints = 0, iapoints = 0, iaoption , playeroption;
 
-
+/*declaração da função select, ela faz com que as imagens sejam selecionadas, adicionando a elas uma classe*/
 function select (op,users) {
     document.getElementById(users+'-option-'+op).classList.add('selected');
 }
 
+/*declaração da função unselect, ela faz com que as imagens sejam deselecionadas, retirando delas uma classe*/
 function unselect (op, users) {
     document.getElementById(users+'-option-'+op).classList.remove('selected');
 }
 
+/*função message que por meio do doom faz com que os textos sejam escritos na caixa*/
 function message(text) {
     document.getElementById('message').innerHTML=text;
   
 }
 
+/*função que alterará o valor da pontuação de cada jogador*/
 function playerPoints () {
     playerpoints++;
     document.getElementById('player-points').innerHTML=playerpoints;
@@ -24,10 +28,12 @@ function iaPoints () {
     document.getElementById('ia-points').innerHTML=iapoints;
 }
 
+/*função que irá sortear um numero para a ia*/
 function iaPlays(max, min) { 
     return Math.floor(Math.random() * 3 + 1);
 }
 
+/*função principal que realiza toda a logica do jogo*/
 function play(op) {
     playeroption = op;
     select(playeroption, 'player')
@@ -37,45 +43,49 @@ function play(op) {
 
     if (playeroption == 1 && iaoption == 1)
     {
-        message('ocorreu um empate!!');
+        message('Ocorreu um empate!!');
     } else if(playeroption == 1 && iaoption == 2){
         playerPoints()
-        message('ponto para você jogador')
+        message('Ponto para você jogador')
     }else if (playeroption == 1 && iaoption == 3) {
         iaPoints()
-        message('ponto para a maquina!!')
+        message('Ponto para a maquina!!')
     }else if (playeroption == 2 && iaoption == 1) {
         iaPoints()
-        message('ponto para a maquina!!')
+        message('Ponto para a maquina!!')
     }else if (playeroption == 2 && iaoption == 2) {
-        message('ocorreu um empate!!');
+        message('Ocorreu um empate!!');
     }else if (playeroption == 2 && iaoption == 3) {
         playerPoints()
-        message('ponto para você jogador')
+        message('Ponto para você jogador')
     }else if (playeroption == 3 && iaoption == 1) {
         playerPoints()
         message('ponto para você jogador')
     }else if (playeroption == 3 && iaoption == 2) {
         iaPoints()
-        message('ponto para a maquina!!')
+        message('Ponto para a maquina!!')
     }else if (playeroption == 3 && iaoption == 3) {
-        message('ocorreu um empate!!');
+        message('Ocorreu um empate!!');
     }
 
 
     setTimeout(function() {
-        unselect(playeroption ,'player');
-        unselect(iaoption ,'ia');
-        message('jogador escolha uma opção...');
+        unselect(1 ,'player');
+        unselect(1 ,'ia');
+        unselect(2 ,'player');
+        unselect(2 ,'ia');
+        unselect(3 ,'player');
+        unselect(3 ,'ia');
+        message('Jogador escolha uma opção...');
       }, 3500);
 
 }
 
-setTimeout(function(){
-    document.getElementById('player-option-1').onclick = function () {play(1)}
-    document.getElementById('player-option-2').onclick = function () {play(2)}
-    document.getElementById('player-option-3').onclick = function () {play(3)}
-} , 3500)
+/*chamada das funções atraves do onclick*/
+document.getElementById('player-option-1').onclick = function () {play(1)}
+document.getElementById('player-option-2').onclick = function () {play(2)}
+document.getElementById('player-option-3').onclick = function () {play(3)}
 
 
+/*mensagem de inicio*/
 message('Bem-vindo caro jogador, você está preparado? Escolha uma opção...');
